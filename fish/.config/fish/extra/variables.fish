4e41493b7cd55bbf9c7de25ee4c987a61eaccad3
Talbet Fulthorpe <talbet.fulthorpe@gmail.com>
2017-12-05 19:28:55 +1100

    fixes

--- fish/.config/fish/extra/variables.fish
+++ fish/.config/fish/extra/variables.fish
@@ -1,5 +1,3 @@
-# Setup your Github Token
-set --export HOMEBREW_GITHUB_API_TOKEN "01010101010101010101010101"
-
 # add /usr/local/sbin to your PATH Variable
 set --export PATH /usr/local/sbin $PATH
+set --export PATH /usr/local/opt/python/libexec/bin $PATH
