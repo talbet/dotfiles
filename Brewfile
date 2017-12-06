6e4c9eb970b6c814dbac3f00c47930e898250be5
Talbet Fulthorpe <talbet.fulthorpe@gmail.com>
2017-12-06 21:43:30 +1100

    Update bootstrap to work with bundle

---
+++ Brewfile
@@ -0,0 +1,90 @@
+# ----
+# TAPS
+# ----
+
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+
+# -----
+# BREWS
+# -----
+
+# Core
+brew 'coreutils'
+brew 'moreutils'
+brew 'findutils'
+brew 'mas'
+brew 'zsh'
+brew 'fish'
+
+# Ruby setup
+brew 'ruby-build'
+brew 'rbenv'
+
+# Python Setup
+# brew install python
+# brew install python3
+
+# Utilities
+brew 'ack'
+brew 'dark-mode'
+brew 'fd'
+brew 'git'
+brew 'lolcat'
+brew 'openssl'
+brew 'ssh-copy-id'
+brew 'stow'
+brew 'svgo'
+brew 'thefuck'
+brew 'tree'
+brew 'wget'
+brew 'youtube-dl'
+
+# -----
+# CASKS
+# -----
+
+# Essentials
+cask '1password'
+cask 'awareness'
+cask 'bartender'
+cask 'bettertouchtool'
+cask 'contexts'
+cask 'dash'
+cask 'karabiner-elements'
+cask 'path-finder'
+cask 'spotify'
+
+# Developer
+cask 'iterm2'
+cask 'visual-studio-code'
+
+# Filesystem
+cask 'dropbox'
+cask 'google-drive-file-stream'
+
+# Others
+cask 'google-chrome'
+cask 'firefox'
+cask 'beaker-browser'
+cask 'macdown'
+cask 'mplayerx'
+cask 'numi'
+cask 'slack'
+cask 'itsycal'
+cask 'station'
+cask 'keybase'
+
+# Fonts
+cask "caskroom/fonts/font-droidsansmono-nerd-font-mono"
+cask "caskroom/fonts/font-hasklig"
+cask "caskroom/fonts/font-source-code-pro"
+cask "caskroom/fonts/font-sourcecodepro-nerd-font-mono"
+
+# ---
+# MAS
+# ---
+
+mas 'Quiver', id: 866773894
+mas 'AntiRSI', id: 442007571
+mas 'Tooth Fairy', id: 1191449274
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,103 +1,36 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
-
-# Ask for the administrator password upfront.
-echo "Run as admin for best results"
-sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
+echo "Updating macOS"
+sudo softwareupdate -i -a
+
+# Install command-line tools
+if [[ "$(xcode-select -p)" == "" ]]; then
+  echo "Installing Xcode Command Line Tools"
+  xcode-select --install
+fi
+
+# Check for Homebrew, Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Make sure we’re using the latest Homebrew.
-brew update
+brew update # Make sure we’re using the latest Homebrew.
+brew upgrade # Upgrade any already-installed formulae.
 
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# Install GNU core utilities (those that come with OS X are outdated).
-brew install coreutils
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install ZSH.
-brew install zsh
 # We installed the new shell, now we have to activate it
-echo "Adding the newly installed shell to the list of allowed shells"
+# echo "Adding the newly installed shell to the list of allowed shells"
 # Prompts for password
-sudo bash -c 'echo /usr/local/bin/zsh >> /etc/shells'
+# sudo bash -c 'echo /usr/local/bin/zsh >> /etc/shells'
 # Change to the new shell, prompts for password
-chsh -s /usr/local/bin/zsh
+# chsh -s /usr/local/bin/zsh
 
-# Install wget
-brew install wget
+if test ! $(brew bundle check); then
+  echo "Installing brews..."
+  brew bundle
+fi
 
-# # Install Python
-# brew install python
-# brew install python3
-
-# Install ruby-build and rbenv
-brew install ruby-build
-brew install rbenv
-
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-brew install git
-brew install lolcat
-brew install openssl
-brew install ssh-copy-id
-brew install stow
-brew install svgo
-brew install thefuck
-brew install tree
-brew install youtube-dl
-
-# Tap Homebrew Cask
-brew tap caskroom/cask
-brew tap caskroom/fonts
-
-# Add fonts
-brew cask install font-hasklig
-brew cask install font-source-code-pro
-brew cask install font-sourcecodepro-nerd-font
-
-# Core casks
-brew cask install 1password
-brew cask install awareness
-brew cask install bartender
-brew cask install bettertouchtool
-brew cask install contexts
-brew cask install dash
-brew cask install flux
-brew cask install karabiner-elements
-brew cask install path-finder
-brew cask install spotify
-
-# Development tool casks
-brew cask install iterm2
-brew cask install visual-studio-code
-
-# Misc casks
-brew cask install google-chrome
-brew cask install firefox
-brew cask install macdown
-brew cask install mplayerx
-brew cask install numi
-brew cask install slack
-brew cask install itsycal
-
-# Disk casks
-brew cask install dropbox
-brew cask install google-drive-file-stream
-
-# Remove outdated versions from the cellar.
-brew cleanup
+brew cleanup # Remove outdated versions
