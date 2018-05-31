e20fd086c08748c14348651a4724a44b99222e2d
Talbet Fulthorpe <talbet.fulthorpe@gmail.com>
2018-05-31 10:51:33 +1000

    Update to name script

--- name.sh
+++ name.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 # Get unique number from MAC address
-ID=$((0x$(ifconfig en0 | awk '/ether/{print $2}'| tail -c 3)))
+UNIQUE_BY_MAC=$(ifconfig en0 | awk '/ether/{print $2}'| cksum | cut -f1 -d" ")
+# UNIQUE_BY_SERIAL=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cksum | cut -f1 -d" ")
 
-# Serial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | md5 | tail -c 3)
-# ID=$((0x${Serial}))
+# Find how many names there are to choose from
+SELECTION_LENGTH=$(cat names | wc -l | xargs)
 
-#Get name from names file
-sed -n "${ID}p" ./names
+# Use modulo to get an ID
+ID=$(($UNIQUE_BY_MAC%$SELECTION_LENGTH))
+
+# Get name from names file
+# sed -n "${ID}p" ./names
+sed "${ID}q;d" ./names
